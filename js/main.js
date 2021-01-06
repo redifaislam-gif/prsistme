098a74cf6b29be513fe50a9b2a99798e6344f16c
Redifa Islam <73694367+redifaislam-gif@users.noreply.github.com>
2021-01-07 01:13:07 +0600

    mmmmmmmmmmmm

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+ function openNav() {
+	  
+	  document.getElementById("myNav").style.width="100%";
+  }
+  
+  
+  function closeNav() {
+	  
+	  document.getElementById("myNav").style.width="0%";
+  }
